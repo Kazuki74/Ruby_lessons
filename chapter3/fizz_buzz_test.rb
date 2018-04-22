d8fab9593e12ea77aa60a019e1a986f6bee1c34b
Kazuki74 <allykakki@me.com>
2018-04-22 09:27:38 +0000

    finished chapter3

---
+++ chapter3/fizz_buzz_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+#自分のファイルパスを起点
+require_relative './fizz_buzz'
+
+class FizzBuzzTest < Minitest::Test
+	def test_fizz_buzz
+		assert_equal '1', fizz_buzz(1)
+		assert_equal '2', fizz_buzz(2)
+		assert_equal 'Fizz', fizz_buzz(3)
+	end
+end
